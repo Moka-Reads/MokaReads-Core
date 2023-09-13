dd6f7add348890ced76570d65865fc5522e9d267
Mustafif <mustafif0929@gmail.com>
2023-09-13 16:00:06 -0400

    added icon suggestion

--- src/resources/cheatsheet.rs
+++ src/resources/cheatsheet.rs
@@ -1,9 +1,10 @@
-use super::Parser as CheatsheetParser;
+use std::collections::HashMap;
+
+use mokareads_macros::EnumVariants;
 use pulldown_cmark::{html, Options, Parser};
 use serde::{Deserialize, Serialize};
 
-use mokareads_macros::EnumVariants;
-use std::collections::HashMap;
+use super::Parser as CheatsheetParser;
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct Cheatsheet {
@@ -80,8 +81,8 @@ impl Level {
 
 impl CheatsheetParser for Cheatsheet {
     fn parse_raw(markdown: &str) -> Self
-    where
-        Self: Sized,
+        where
+            Self: Sized,
     {
         let separator = "---";
         let mut sections = markdown.splitn(3, separator);
@@ -102,8 +103,8 @@ impl CheatsheetParser for Cheatsheet {
     }
 
     fn parse(markdown: &str) -> Self
-    where
-        Self: Sized,
+        where
+            Self: Sized,
     {
         let mut cheatsheet = Self::parse_raw(markdown);
         let parser = Parser::new_ext(cheatsheet.content.as_str(), Options::all());
@@ -116,16 +117,16 @@ impl CheatsheetParser for Cheatsheet {
         let parser = Parser::new_ext(&self.content, Options::all());
         let mut html_output = String::new();
         html::push_html(&mut html_output, parser);
-        Self{
+        Self {
             metadata: self.metadata.clone(),
             slug: self.slug.to_string(),
-            content: html_output
+            content: html_output,
         }
     }
 }
 
 #[derive(
-    Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Deserialize, Serialize, EnumVariants, Hash,
+Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Deserialize, Serialize, EnumVariants, Hash,
 )]
 pub enum Language {
     Kotlin,
@@ -152,6 +153,19 @@ impl Language {
             "swift" => Language::Swift,
             "go" => Language::Go,
             _ => Language::Other,
+        }
+    }
+    pub fn icon_suggestion(&self) -> String {
+        match self {
+            Language::Kotlin => "devicon-kotlin-plain".to_string(),
+            Language::Rust => "devicon-rust-plain".to_string(),
+            Language::C => "devicon-c-plain".to_string(),
+            Language::CPP => "devicon-cplusplus-plain".to_string(),
+            Language::Zig => "devicon-zig-original".to_string(),
+            Language::Python => "devicon-python-plain".to_string(),
+            Language::Swift => "devicon-swift-plain".to_string(),
+            Language::Go => "devicon-go-original-wordmark".to_string(),
+            Language::Other => "devicon-github-original".to_string(),
         }
     }
 }
