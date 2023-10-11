2b6d4743672db057a42b017ad26e3e5be18d01cc
Mustafif <mustafif0929@gmail.com>
2023-10-10 23:07:58 -0400

    better documentation, added branches to api

--- src/api.rs
+++ src/api.rs
@@ -1,18 +1,37 @@
 use crate::Result;
 
+/// Different API Handles to send requests to
 #[derive(Debug, Clone, Copy)]
 pub enum Api {
+    /// Requests for the complete resources index
     Resources,
+    /// Requests for the cheathseet index
     Cheatsheets,
+    /// Requests for the articles index
     Articles,
+    /// Represents for the guides index
     Guides,
+    /// Represents the awesome lists index
     Awesome,
+    /// Requests for the cheatsheet index as `HashMap<Language, Vec<Cheatsheet>>`
     LangMap,
 }
 
+/// The MoKa Reads branch either (stable or beta)
+#[derive(Debug, Clone, Copy)]
+pub enum Branch{
+    /// Represents the `main` branch in `git`
+    Stable,
+    /// Represents the `beta` branch in `git`
+    Beta
+}
+
 impl Api {
-    fn link(&self) -> String {
-        let base = "https://mokareads.org/api/";
+    fn link(&self, branch: Branch) -> String {
+        let base = match branch{
+            Branch::Stable => "https://mokareads.org/api/",
+            Branch::Beta => "https://beta.mokareads.org/api/"
+        };
         match self {
             Api::Resources => format!("{}resources", base),
             Api::Cheatsheets => format!("{}cheatsheets", base),
@@ -22,9 +41,10 @@ impl Api {
             Api::LangMap => format!("{}lang_map", base),
         }
     }
-    pub async fn get(&self) -> Result<String> {
-        let link = self.link();
+    pub async fn get(&self, branch: Branch) -> Result<String> {
+        let link = self.link(branch);
         let response = reqwest::get(&link).await?.text().await?;
         Ok(response)
     }
 }
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,9 @@
-/*
-MoKa Reads Core Library
-Developed by Mustafif Khan
 
-This is built to serve as a core library for
-various MoKa Reads application/tools.
-
-All code is under the GPLv2 License (LICENSE.md).
- */
+//! # MoKa Reads Core Library
+//! **Developed by Mustafif Khan**
+//!
+//! This is built to serve as a core library for various MoKa Reads application/tools.
+//! All code is under the GPLv2 License
 
 /// A wrapper over the MoKa Reads API spec
 pub mod api;
@@ -18,7 +15,7 @@ pub mod latex;
 /// The different MoKa Reads Resources
 pub mod resources;
 
-pub use rss::*;
+pub use rss::{Item, Channel};
 
 /// A generic Result type
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
